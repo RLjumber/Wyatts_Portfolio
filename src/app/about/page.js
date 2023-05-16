2e0d19cedbd8da76b2e0ba089921d7081fd5afc2
RLjumber <ronaldjumber@gmail.com>
2023-05-16 18:53:40 -0400

    more mobile layout work, created components folder to house Type component and a header component that I plan on making next for in-site navigation on pages other than the home page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,7 +7,7 @@ export default function About() {
             <div className={styles.content}>
                 <h1>About Me</h1>
                 <div className={styles.picture_and_text}>
-                    <Image src={"/wyatt_profile.jpg"} width={453} height={604} alt="Probably needs a haircut"/>
+                    <Image className={styles.image} src={"/wyatt_profile.jpg"} width={453} height={604} alt="Probably needs a haircut"/>
                     <p>Hi I’m Wyatt Wingate, a 3D technical animation student at SCAD with interest in animation and modeling for video games and movies, and wanting to specialize in hard surface modeling and design. In my free time I play video games, build and paint scale models from model tanks, to Warhammer and DND models and I enjoy spending time with friends and family. My art journey began with ceramics in high school through all four years ending in AP ceramics and winning the art award for my school, and found a way to merge my passions of gaming and 3D modeling by discovering blender and zbrush, and enrolled into school shortly after.</p>
                 </div>
                 </div>
