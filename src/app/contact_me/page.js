9b3946d9ab8f248ff73d653570823f6e55f68b44
RLjumber <ronaldjumber@gmail.com>
2023-05-16 20:37:14 -0400

    in site navigation complete with some pretty cool ternary statementsto render the two nav links that arent currently active using the usePathname under the use client next.js statement

--- src/app/contact_me/page.js
+++ src/app/contact_me/page.js
@@ -1,25 +1,29 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import Header from "../components/header/Header";
 
 export default function ContactMe() {
     return (
-        <div className={styles.main_div}>
-            <div className={styles.content}>
-                <h1>Contact Me</h1>
-                {/* <p>Could put something here?</p> */}
+        <>
+            <Header />
+            <div className={styles.main_div}>
+                <div className={styles.content}>
+                    <h1>Contact Me</h1>
+                    {/* <p>Could put something here?</p> */}
+                </div>
+                <div className={styles.second_div}>
+                    <video className={styles.logo_animation} autoPlay muted>
+                    <source src="/Comp_1RGB.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                    </video>
+                    <ul className={styles.list}>
+                        <Link href="https://www.artstation.com/wyattwingate8" className={styles.link}><li>My Art Station</li></Link>
+                        <li>Email: walkerwyatt1213@gmail.com</li>
+                        <li>Insta: <em>wyatt_w27</em> or <em>rekragart</em></li>
+                        <li>Phone: (904) 309 1087</li>
+                    </ul>
+                </div>
             </div>
-            <div className={styles.second_div}>
-                <video className={styles.logo_animation} autoPlay muted>
-                <source src="/Comp_1RGB.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-                </video>
-                <ul className={styles.list}>
-                    <Link href="https://www.artstation.com/wyattwingate8" className={styles.link}><li>My Art Station</li></Link>
-                    <li>Email: walkerwyatt1213@gmail.com</li>
-                    <li>Insta: <em>wyatt_w27</em> or <em>rekragart</em></li>
-                    <li>Phone: (904) 309 1087</li>
-                </ul>
-            </div>
-        </div>
+        </>
     )
 };
