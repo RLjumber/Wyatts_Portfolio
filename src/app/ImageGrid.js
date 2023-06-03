de209dd449ac1875b59262811da1bb578c1b3009
RLjumber <ronaldjumber@gmail.com>
2023-06-03 16:39:10 -0400

    blurred image when hover o grid item

--- src/app/ImageGrid.js
+++ src/app/ImageGrid.js
@@ -10,98 +10,108 @@ export default function ImageGrid() {
             <div className={styles.image_grid}>
                 <div className={styles.image_grid_item}>
                     <Link href="/cybernoir">
-                        <div className={styles.image_wrapper}>
                             <Image
                                 className={styles.image}
                                 src={"/CharcoalNoir.jpg"}
                                 alt="CharcoalNoir"
                                 fill
                             />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Cyber-Noir</div>
+                            </div>
+                        
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/cybercity">
-                        <div className={styles.image_wrapper}>
+                        
                             <Image 
                                 className={styles.image}
                                 src={"/Cybercity.jpg"}
                                 alt="Cybercity"
                                 fill 
                                 />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Cyber-City</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
-                    <Link href="/cyberstreetcorner">
-                        <div className={styles.image_wrapper}>
-                            <Image
+                    <Link href="/cyberstreetcorner">                            <Image
                                 className={styles.image}
                                 src={"/CyberStreetCorner.jpg"}
                                 alt="CyberStreetCorner"
                                 fill
                             />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Cyberstreet Corner</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/dualangel">
-                        <div className={styles.image_wrapper}>
                             <Image 
                                 className={styles.image} 
                                 src={"/Dual_Angel.jpg"} 
                                 alt="Dual Angel" 
                                 fill
                                 />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Dual Angel</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/swordlord">
-                        <div className={styles.image_wrapper}>
                             <Image
                                 className={styles.image}
                                 src={"/Sword_Lord.jpg"}
                                 alt="Hidetaka Miyazaki Inspiration"
                                 fill
                             />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Sword Lord</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/bladerunner">
-                        <div className={styles.image_wrapper}>
                             <Image
                                 className={styles.image}
                                 src={"/BladeRunnerThumbnail.jpg"}
                                 alt="BladeRunnerThumbnail.jpg"
                                 fill
                             />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Bladerunner Animation</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/poster">
-                        <div className={styles.image_wrapper}>
                             <Image
                                 className={styles.image}
                                 src={"/WyattWingate-Poster-Final.jpg"}
                                 alt="Hidetaka Miyazaki Inspiration"
                                 fill
                             />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Hidetaka Miyazaki Inspiration</div>
+                            </div>
                     </Link>
                 </div>
                 <div className={styles.image_grid_item}>
                     <Link href="/gundam">
-                        <div className={styles.image_wrapper}>
                             <Image 
                                 className={styles.image} 
                                 src={"/Gundam.jpg"} 
                                 alt="Gundam" 
                                 fill
                                 />
-                        </div>
+                            <div className={styles.image_blur}> 
+                                <div className={styles.image_hover_title}>Unit 07</div>
+                                <div></div>
+                            </div>  
                     </Link>
                 </div>
             </div>
