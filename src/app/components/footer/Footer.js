2b0a48de6f2ff3583564a9893f796d702eee95de
RLjumber <ronaldjumber@gmail.com>
2023-06-06 04:09:42 -0400

    about me section on main page passable, icons for contact me and footer also added. need to link the icons to destinations and also update the other tabs

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,34 +1,34 @@
 import styles from "./Footer.module.css";
+import { AiFillEdit, AiFillInstagram, AiFillPhone } from "react-icons/ai";
+import { GrMail } from "react-icons/gr";
 
 export default function Footer() {
     return (
-        <div className={styles.footer_main}>
-            <div className={styles.list_box}>
-                <h3>Title</h3>
-                <ul>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                </ul>
+        <div className={styles.main}>
+            <div className={styles.contact}>
+                <h1>Like what you see? Contact Me!</h1>
+                <div className={styles.icons}>
+                    <AiFillEdit />
+                    <GrMail />
+                    <AiFillInstagram />
+                    <AiFillPhone />
+                </div>
             </div>
-            <div className={styles.list_box}>
-                <h3>Title</h3>
-                <ul>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                </ul>
-            </div>
-            <div className={styles.list_box}>
-                <h3>Title</h3>
-                <ul>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                    <li>Word</li>
-                </ul>
+            <div className={styles.footer_main}>
+                <div className={styles.list_box}>
+                    <h3>Last Updated: June 2023</h3>
+                </div>
+                <div className={styles.list_box}>
+                    <h3>Savannah College of Art and Design</h3>
+                </div>
+                <div className={styles.list_box}>
+                    <h3>Tech and Credit</h3>
+                    <ul>
+                        <li>Built with Next.js 13</li>
+                        <li>Hosting by Vercel</li>
+                        <li>Built by R. Luke Jumber </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
