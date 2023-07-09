8e7b21da42f5b6025d79e88c5e65cbc9f578ab62
RLjumber <ronaldjumber@gmail.com>
2023-07-09 15:34:05 -0400

    Footer updates, links working and phone/emal alerts in place

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,22 +1,30 @@
+"use client"
+
+import Link from "next/link";
 import styles from "./Footer.module.css";
 import { AiFillEdit, AiFillInstagram, AiFillPhone } from "react-icons/ai";
 import { GrMail } from "react-icons/gr";
 
 export default function Footer() {
+
+    const showPhoneNumber = () => {
+       alert("Call or Text at (904) 309 1087")
+    };
+
     return (
         <div className={styles.main}>
             <div className={styles.contact}>
                 <h1>Like what you see? Contact Me!</h1>
                 <div className={styles.icons}>
-                    <AiFillEdit />
-                    <GrMail />
-                    <AiFillInstagram />
-                    <AiFillPhone />
+                    <Link href="https://www.artstation.com/wyattwingate8"><AiFillEdit /></Link>
+                    <Link href="mailto:walkerwyatt1213@gmail.com"><GrMail /></Link>
+                    <Link href="https://www.instagram.com/rekragart/"><AiFillInstagram /></Link>
+                    <AiFillPhone onClick={showPhoneNumber} />
                 </div>
             </div>
             <div className={styles.footer_main}>
                 <div className={styles.list_box}>
-                    <h3>Last Updated: June 2023</h3>
+                    <h3>Last Updated: July 10th, 2023</h3>
                 </div>
                 <div className={styles.list_box}>
                     <h3>Savannah College of Art and Design</h3>
